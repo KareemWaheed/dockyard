feb4406adb7aeeb07c9f9aa3add8039749145515
Kareem Waheed <kareemwaheed94@gmail.com>
2026-03-26 02:23:17 +0200

    fix: remove hardcoded CORS origin restriction

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const http = require('http');
 require('./db');
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 app.use(express.json());
 
 // Routes
